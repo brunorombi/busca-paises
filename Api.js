01ad44e6a8122f6d5d262c45646242d01f099253
Bruno Rombi <brunorombi@yahoo.com>
2026-05-27 12:51:36 -0300

    feat(api): adiciona api restcountries com busca por nome

---
+++ Api.js
@@ -0,0 +1,16 @@
+const API = "https://restcountries.com/v3.1/name";
+
+export default async function buscaPais(nome) {
+    try {
+        const response = await fetch(`${API}/${nome}`);
+
+        if(!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}?`)
+        }
+
+        const data = await response.json();
+        console.log(data);
+    } catch(error) {
+        console.error(error);
+    }
+}
--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import buscaPais from './Api';
 
 export default function App() {
+  buscaPais('brazil');
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
